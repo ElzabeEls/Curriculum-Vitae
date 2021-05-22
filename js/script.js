65d8a99fbc9e5717c3c3fc0f07286f4aefe80e69
Elzabé Els <53035581+ElzabeEls@users.noreply.github.com>
2021-05-23 00:09:31 +0200

    Google Maps API Styled Map Works!

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-function initMap(){
-  map = new google.maps.Map(document.getElementById("map"),options);
+function initMap() {
+          var myLatLng = {lat: -34.075691, lng: 18.843266};
 
-    var options = {
-      center: {lat: 38.3460, lng:-0.4907},
-      zoom: 8,
-      mapId:'da1bff2d63c7a5de'
-    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 9,
+      center: myLatLng,
+      mapId:'da1bff2d63c7a5de',
+      disableDefaultUI: true
+      });
 
     var marker = new google.maps.Marker({
-      position:{lat:37.9922, lng: -1.1307},
-      map:map,
-      icon:"https://img.icons8.com/nolan/2x/marker.png"
+      position: myLatLng,
+      map: map,
+      icon:"images/clipart2350691.png"
     });
 
-    //marker.addListerner("mouseover", () => {
-    //  detailWindow.open(map, marker);
-  //  });
   }
+
+
+        google.maps.event.addDomListener(window, 'load', initialize);
